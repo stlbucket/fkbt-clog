6d4c3a497b1072bdb28fa7dc0143bda324459ec5
stlbucket <stlbucket@gmail.com>
2017-04-06 10:21:13 -0500

    1.0.3

--- src/clogError/index.js
+++ src/clogError/index.js
@@ -1,5 +1,10 @@
 const clog = require('../clog');
 
-module.exports = function (sectionIdentifier, logItem) {
-  return clog(sectionIdentifier, logItem, 'red')
+module.exports = function (sectionIdentifier, logItem, exitProcess) {
+  if (exitProcess) {
+    clog(sectionIdentifier, logItem, 'yellow');
+    process.exit();
+  } else {
+    return clog(sectionIdentifier, logItem, 'yellow');
+  }
 }
